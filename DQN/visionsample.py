98aeca5e3accc0e7468e7ec0101ed1ce92311fee
clmnlab <42018654+clmnlab@users.noreply.github.com>
2021-09-01 15:23:58 +0900

    Delete visionsample.py

--- DQN/visionsample.py
+++
@@ -1,9 +0,0 @@
-import torchvision
-import numpy as np
-from PIL import Image as pilimg
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
-image = pilimg.open('./a.bmp').convert('L')
-plt.imshow(image, cmap= 'gray')
-plt.show()
